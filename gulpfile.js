0d117204e1daa617f7313ff7d38028b922139e9f
Asa Smith <smith.asa.la@gmail.com>
2015-09-30 18:15:03 -0400

    9/30

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp')
+
+gulp.task('myTask', function() {
+  console.log('Running my task!');
+});
+
+var gulp = require('gulp'),
+    gls = require('gulp-live-server');
+
+    gulp.task('server', function() {
+        var server = gls('.');
+        server.start().then(function(result) {
+            console.log('Server exited with result:', result);
+        });
+        return gulp.watch(['index.js', 'models/*.js'], function(file) {
+          console.log(file);
+            server.start.apply(server);
+        });
+    });
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+//require('express-debug')(app, {});
+
 app.use('/bower_components',
   express.static(__dirname + '/bower_components'));
 app.use('/public', express.static(__dirname + '/public'));
@@ -9,26 +11,64 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+var Sequelize = require('sequelize');
+var SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+var sequelize = new Sequelize(
+  "database",
+  "username",
+  "password", {
+    "dialect": "sqlite",
+    "storage": "./store/session.sqlite"
+  });
+
+var store = new SequelizeStore({ db: sequelize });
+store.sync();
+
+app.use(cookieParser());
+app.use(session({
+  saveUninitialized: false,
+  resave: false,
+  secret: 'I see dead people',
+  store: store
+}));
+
+app.use(require('flash')());
+
+//app.use(require('morgan')('dev'));
+
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-  //res.send('Hello world!');
   res.render('index');
 });
 
 app.get('/game', function(req, res) {
-  res.render('game', req.query);
+  var playerName = req.session.playerName;
+  res.render('game', { username: playerName });
 });
 
 app.post('/game', function(req, res) {
-  res.redirect('/game?username=' + req.body.username);
+  req.session.playerName = req.body.username;
+  req.session.save(function() {
+    res.redirect('/game');
+  });
 });
 
 var models = require('./models');
 
 app.get('/games', function(req, res) {
     models.Board.findAll().then(function(boards) {
-      res.render('games', { boards: boards });
+      if (req.session.user_id) {
+        models.Users.findById(req.session.user_id).then(function(user) {
+          res.render('games', { boards: boards, user: user });
+        })
+      } else {
+        res.render('games', { boards: boards });
+      }
     });
 });
 
@@ -44,8 +84,36 @@ app.post('/games', function(req, res) {
             res.redirect('/games/' + board.id);
         })
         .catch(function(errors) {
-            res.send(JSON.stringify(errors))
+          models.Board.findAll().then(function(boards) {
+            res.render('games', { boards: boards, errors: errors });
+          });
         });
+});
+
+// User registration
+app.get('/register', function(req, res) {
+  res.render('register');
+});
+
+app.post('/register', function(req, res) {
+  // Does the user exist already?
+  models.Users.find({ where: { user_name: req.body.username }})
+    .then(function(user) {
+        if (user) {
+          req.flash('warning', "Username already exists");
+          req.session.save(function() {
+            res.redirect('/register');
+          });
+        } else {
+          models.Users.create(req.body)
+            .then(function(newUser) {
+              req.session.user_id = newUser.id;
+              req.session.save(function() {
+                res.redirect('/games');
+              });
+            });
+        }
+    });
 });
 
 var server = app.listen(3000, function() {
